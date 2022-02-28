f3e9de4a1ca8f61cbd91d235cd1b6d1b1c10e4db
GAMD Solutions <admin@gamdsolutions.com>
2022-02-28 15:06:28 +0300

    PostgreSql to MySql conversion

--- Projects/TIN Checker/tinchecker/__init__.py
+++ Projects/TIN Checker/tinchecker/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, current_user
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://gamdsolu_tinchecker_admin:tinchecker_admin_password@localhost:5432/gamdsolu_tinchecker_db'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:root_password@localhost/gamdsolu_tinchecker_db'
 app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
